7603572868ecad66d2f2bb58eb74c022abd7f1f5
me <tldbs331@gmail.com>
2022-11-30 18:28:24 +0900

    2022-11-30

---
+++ HELLO_PYTHON/day05/main01.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+from_class = uic.loadUiType("main01.ui")[0]
+class MyWindow(QMainWindow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self) #this가 상속받은곳에 없으면 부모를 찾아감
+        
+        self.btn.clicked.connect(self.f_click)
+        
+        
+    def f_click(self):
+        self.label.setText("Good Evening")
+        
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mywindow = MyWindow()
+    mywindow.show()
+    app.exec_()
