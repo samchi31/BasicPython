9c7f4cf017d3fed2a524c410b355796f2aba2d50
me <tldbs331@gmail.com>
2022-12-15 13:55:03 +0900

    2022-12-15

--- HELLO_PYTHON/day17/myflask.py
+++ HELLO_PYTHON/day17/myflask.py
@@ -7,26 +7,44 @@ app.secret_key = "mysecret"
 
 socket_io = SocketIO(app)
 
+@app.route('/chat2')
+def chat2():
+    return render_template('chat2.html')
+
 @app.route('/')
-@app.route('/chat')
-def chatting():
-    return render_template('chat.html')
+@app.route('/allready')
+def allready():
+    return render_template('allready.html')
+
+@app.route('/canvas')
+def canvas():
+    return render_template('canvas.html')
 
 
 @socket_io.on("message")
 def request(message):
     print("message : "+ message)
-    to_client = dict()
-    if message == 'new_connect':
-        to_client['message'] = "새로운 유저가 난입하였다!!"
-        to_client['type'] = 'connect'
-    else:
-        to_client['message'] = message
-        to_client['type'] = 'normal'
-    # emit("response", {'data': message['data'], 'username': session['username']}, broadcast=True)
-    send(to_client, broadcast=True)
+    mydict = dict()
+    mydict['message'] = message
+    send(mydict, broadcast=True)
 
 
 
 if __name__ == '__main__':
-    socket_io.run(app, debug=True, port=9999)
+    socket_io.run(app, debug=True,host="0.0.0.0",port=9999)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
