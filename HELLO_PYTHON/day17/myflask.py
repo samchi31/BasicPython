5e961542bd387fcf46094a7b976595b69df2c0c7
me <tldbs331@gmail.com>
2022-12-19 13:55:20 +0900

    2022-12-19

--- HELLO_PYTHON/day17/myflask.py
+++ HELLO_PYTHON/day17/myflask.py
@@ -35,7 +35,7 @@ def request(message):
 
 
 if __name__ == '__main__':
-    socket_io.run(app, debug=True,host="0.0.0.0",port=9999)
+    socket_io.run(app, debug=True,host="0.0.0.0",port=5000)
     
     
     
